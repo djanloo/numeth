2e959365f84edb5eac2007237d7b8b97d8a6ea9e
djanloo <zzibecu@gmail.com>
2022-11-18 12:43:16 +0100

    Manually rebased commit

--- test.py
+++ test.py
@@ -13,14 +13,26 @@ from numeth.ising import ising, set_seed, energy
 from matplotlib.animation import FuncAnimation
 from time import time
 from rich.progress import track
+# from serverstd import serverStderr, serverStdout, splot
+import sys
+
+# sys.stdout = serverStdout()
+# sys.stderr = serverStderr()
+
+def autocorr(y):
+    x = y.copy()
+    x -= np.mean(x)
+    x /= np.std(x)
+    result = np.correlate(x, x, mode='full')/len(x)
+    return result[result.size//2:]
 
 # Imposta il seed usando il tempo
 t = time()
 set_seed( int((t- int(t))*10000) )
 
-N_celle = 200
+N_celle = 32
 n_temperature = 30
-n_samples = 10
+n_samples = 1000
 
 psi = np.zeros((n_temperature, n_samples))
 H = np.zeros((n_temperature, n_samples))
@@ -30,21 +42,24 @@ sigma_psi = np.zeros(n_temperature)
 mean_H = np.zeros(n_temperature)
 sigma_H = np.zeros(n_temperature)
 
-T = np.linspace(1.5, 3.0, n_temperature)
+T = np.linspace(-1.0, 1.0, n_temperature)
+T = 2.3*(T**3 + 1) + 1e-3
 
 for i, t in track(enumerate(T), total=n_temperature):
-   
-    # Field limit + thermalization
-    S = ising(N=N_celle, beta=1/t, J=1.031, h=0.1, N_iter=128)
-    for h in [0.003, 0.002, 0.001, 0.0, 0.0, 0.0]:
-        S = ising(N=N_celle, beta=1/t, J=1.0, h=h, N_iter=10, startfrom=S)
+    print(f"elapsed temperatures: {int(i/len(T)*100)}%")
+    S = ising(N=N_celle, beta=1/t, J=1.031, h=0.0, N_iter=100)
 
     for sample in range(n_samples):
-        psi[i, sample] = np.mean(S)
+        psi[i, sample] = np.abs(np.mean(S))
         H[i, sample] = energy(S,1.0,0.0)
-        ising(N=N_celle, beta=1/t, J=1.0, h=0.0 , N_iter=256, startfrom=S)
+        ising(N=N_celle, beta=1/t, J=1.0, h=0.0 , N_iter=50, startfrom=S)
 
-mean_psi = np.mean(psi, axis=1)
+print("saving")
+np.save(f"psi_L{N_celle}_nT{n_temperature}_nsamp{n_samples}.npy", psi)
+np.save(f"H_L{N_celle}_nT{n_temperature}_nsamp{n_samples}.npy", H)
+
+
+mean_psi = np.abs(np.mean(psi, axis=1))
 sigma_psi = np.std(psi, axis=1)
 mean_H = np.mean(H, axis=1)
 sigma_H = np.std(H, axis=1)
@@ -57,15 +72,14 @@ ax[0].set_ylabel(r"$\langle \psi \rangle$")
 ax[1].errorbar(T, mean_H, sigma_H, ls="", marker=".")
 ax[1].set_ylabel(r"$\langle H \rangle$")
 
-plt.savefig("psi_H.png")
+# splot(fig)
 
 fig, ax = plt.subplots(2, sharex=True)
-ax[0].plot(T, (sigma_psi**2)/T, ls="", marker=".")
+ax[0].plot(T, (sigma_psi**2), ls="", marker=".")
 ax[0].set_xlabel("T")
 ax[0].set_ylabel(r"$\langle \chi \rangle$")
 
-ax[1].plot(T, (sigma_H**2)/T ,  ls="", marker=".")
+ax[1].plot(T, (sigma_H**2) ,  ls="", marker=".")
 ax[1].set_ylabel(r"$\langle C_v \rangle$")
 
-plt.savefig("chi_cv.png")
-
+# splot(fig)
